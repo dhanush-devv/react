c51e76af7e68b7ed2e99ea0d4e2bac3f565914b6
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-14 11:46:37 +0530

    commit

---
+++ 2-counter/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  function increment() {
+    if(count>=0){
+    setCount(count+1)
+    }
+    
+  }
+  function decrement() {
+    if(count>0){
+    setCount(count-1)
+    }
+    
+  }
+  return (
+    <div className='flex flex-col justify-center items-center h-screen bg-gray-700 text-white'>
+      <h1>Counter:{count}</h1>
+      <button onClick={increment}  className='bg-green-400 rounded p-4 text-white text-2xl hover:text-black cursor-pointer mt-5'>Increment</button>
+      <button onClick={decrement} className='bg-red-400 rounded p-3 text-white text-2xl hover:text-black cursor-pointer mt-5'>Decrement</button>
+    </div>
+  )
+}
+
+export default App
